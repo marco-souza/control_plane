eb20ac71198e3c3c6012a07c93de041b108ccaf1
Marco Antônio <ma.souza.junior@gmail.com>
2026-07-08 07:48:25 -0300

    fea: fetch persisted URL

--- lib/control_plane/rss/feed/worker.ex
+++ lib/control_plane/rss/feed/worker.ex
@@ -21,7 +21,24 @@ defmodule ControlPlane.RSS.Feed.Worker do
 
   @impl true
   def handle_info(:fetch, %Source{} = source) do
-    Logger.info("[feed][worker] TODO: fetch content for #{source.url}")
+    Logger.info("[feed][worker] fetch content for #{source.url}")
+
+    normalized_url = normalize_url(source)
+
+    case Req.get(normalized_url) do
+      {:ok, %{status: 200, body: body}} ->
+        Logger.info("[feed][worker] content fetched: #{inspect(body)}")
+
+      # TODO: ensure XML is a valid rss
+      # TODO: implement XML parsing
+      # TODO: add posts to the DB
+
+      {:ok, %{status: 404}} ->
+        Logger.info("[feed][worker] content not found: #{source.url}")
+
+      {:error, reason} ->
+        Logger.info("[feed][worker] content not found: #{inspect(reason)}")
+    end
 
     schedule_fetch(source.interval)
 
@@ -34,5 +51,15 @@ defmodule ControlPlane.RSS.Feed.Worker do
     Logger.info("[feed][worker] scheduling next execution: #{next_execution}")
 
     Process.send_after(self(), :fetch, next_execution)
+  end
+
+  defp normalize_url(%Source{} = source) do
+    case source.url do
+      "http" <> _ ->
+        source.url
+
+      _ ->
+        "https://#{source.url}"
+    end
   end
 end
